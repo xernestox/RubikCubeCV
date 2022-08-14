cc68bf3d05614bab5830d35c0f9019aa2496dd20
ernesto99rdz <ernesto99rdz@live.com>
2022-08-14 17:13:38 -0500

    OpenCV Instalado

---
+++ main.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+from cv2 import destroyAllWindows
+
+img = cv2.imread("./rubik-cube.jpg", cv2.IMREAD_COLOR)
+window_name = 'Image'
+start_point = (5,5)
+end_point = (220,220)
+color = (255,0,0)
+thickness = 2
+
+img = cv2.rectangle(img, start_point, end_point, color, thickness)
+
+cv2.imshow("image", img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
