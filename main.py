347038d3472f4f8a5aa20339e320ee3e3a3a1e22
ernesto99rdz <ernesto99rdz@live.com>
2022-08-15 00:03:39 -0500

    Rectangulo Ajustado

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from cv2 import destroyAllWindows
 
 img = cv2.imread("./rubik-cube.jpg", cv2.IMREAD_COLOR)
 window_name = 'Image'
-start_point = (5,5)
-end_point = (220,220)
+start_point = (50,50)
+end_point = (90,90)
 color = (255,0,0)
 thickness = 2
 
