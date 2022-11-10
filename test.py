0963eefce3dd59e37afe8f0c7c86181d3555ba26
ernesto99rdz <ernesto99rdz@live.com>
2022-11-10 01:36:07 -0600

    does read arduino

--- test.py
+++ test.py
@@ -1,19 +1,17 @@
 import serial
 import time
 
-arduino = serial.Serial(port='COM3', baudrate=115200, timeout=.1)
+arduino = serial.Serial(port='COM3', baudrate=19200, timeout= 1.0)
 
 def write_read(x):
     arduino.write(x.encode())
-    time.sleep(0.05)
+    # time.sleep(0.05)
     data = arduino.readline()
     return data
 
-commands = list("26426364277426277227727272774372774277226427736427742773772277438888")
-print(commands)
+commands = list("11111111111111111111")
 
 for x in commands:
-    time.sleep(0.2)
     value = write_read(x)
     print(value)
 
