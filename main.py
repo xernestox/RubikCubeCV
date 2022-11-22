c549b73f751d564ded24a559b3bbd3e3801dfe7e
ernesto99rdz <ernesto99rdz@live.com>
2022-11-22 13:30:10 -0600

    Entrega FInal

--- main.py
+++ main.py
@@ -4,66 +4,66 @@ from motor_commands import arduino_commands
 from send2arduino import send_commands
 import time
 
-colors_array = [None] * 6
-centers_array = [None] * 6
+# colors_array = [None] * 6
+# centers_array = [None] * 6
  
-#detect up
-send_commands("x1")
-time.sleep(4)
+# # detect up
+# send_commands("x1")
+# time.sleep(4)
 
-colors_array[0], centers_array[0] = detect_colors()
-print(colors_array[0], centers_array[0])
+# colors_array[0], centers_array[0] = detect_colors()
+# print(colors_array[0], centers_array[0])
 
-#detect right
+# # #detect right
 
-send_commands("x9125")
-time.sleep(5)
-colors_array[1], centers_array[1] = detect_colors()
-print(colors_array[1], centers_array[1])
+# send_commands("x9125")
+# time.sleep(5)
+# colors_array[1], centers_array[1] = detect_colors()
+# print(colors_array[1], centers_array[1])
 
-#detect back
+# # #detect back
 
-send_commands("x912")
-time.sleep(4)
-colors_array[2], centers_array[2] = detect_colors()
-print(colors_array[2], centers_array[2])
+# send_commands("x912")
+# time.sleep(4)
+# colors_array[2], centers_array[2] = detect_colors()
+# print(colors_array[2], centers_array[2])
 
-#detect left
+# # #detect left
 
-send_commands("x912")
-time.sleep(4)
-colors_array[3], centers_array[3] = detect_colors()
-print(colors_array[3], centers_array[3])
+# send_commands("x912")
+# time.sleep(4)
+# colors_array[3], centers_array[3] = detect_colors()
+# print(colors_array[3], centers_array[3])
 
-#detect front
+# #detect front
 
-send_commands("x912")
-time.sleep(4)
-colors_array[4], centers_array[4] = detect_colors()
-print(colors_array[4], centers_array[4])
+# send_commands("x912")
+# time.sleep(4)
+# colors_array[4], centers_array[4] = detect_colors()
+# print(colors_array[4], centers_array[4])
 
-#detect down
+# #detect down
 
-send_commands("x91425")
-time.sleep(6)
-colors_array[5], centers_array[5] = detect_colors()
-print(colors_array[5], centers_array[5])
+# send_commands("x91425")
+# time.sleep(6)
+# colors_array[5], centers_array[5] = detect_colors()
+# print(colors_array[5], centers_array[5])
 
-#reset to initial position
-send_commands("x915224")
-time.sleep(6)
+# #reset to initial position
+# send_commands("x915224")
+# time.sleep(6)
 
 #Up Right Back Left Front Down
 
-# colors_array = ["GGOGGBRBO","WWWYWWYYW","BOOROOROO","YWYYYWYYW","BRGORGBRR","RGGBBRBBG"]
-# centers_array = ["G","W","O","Y","R","B"]
+colors_array = ['GWGGWGGWO', 'YRRWBWWRR', 'WBWOOOBBW', 'OOOYGYOOY', 'YGBGRRGRR', 'RBBBYYBYY']
+centers_array = ['W', 'B', 'O', 'G', 'R', 'Y']
 
 #solve cube with kociemba algorithm
 kociemba_array = solve_cube(colors_array, centers_array)
 
 #transforms kociemba algorithm to arduino commands
 arduino_array = arduino_commands(kociemba_array)
-arduino_array = "x" + arduino_array
+arduino_array = "x1" + arduino_array
 print(arduino_array)
 
 #sends the commands to the arduino
